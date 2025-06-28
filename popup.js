efcefa860646b3eec7990fdf2ff640abffbb1c08
Michael Huelsen <github@michael-huelsen.de>
2025-06-28 21:53:35 +0200

    updated

--- popup.js
+++ popup.js
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-const SUPPORT_URL = "https://www.proginosko.com/leechblock/support/";
+const SUPPORT_URL = "https://www.incuvolve.de./ivblock/support/";
 
 // Initialize page
 //
